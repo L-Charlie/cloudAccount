b60cea54c15664138d06ddb225cbfdb039e7565d
mh1916488432@163.com <mh1916488432@163.com>
2021-02-19 22:26:30 +0800

    function

---
+++ CloudAccount/app/src/main/java/com/example/cloudaccount/ListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.cloudaccount;
+
+public class ListAdapter {
+}
---
+++ CloudAccount/app/src/main/java/com/example/cloudaccount/new_cost.java
@@ -0,0 +1,14 @@
+package com.example.cloudaccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_cost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_cost);
+    }
+}
